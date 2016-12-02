3ccbb2745daf5869d489af06a482345e8723f938
simon <simon@DESKTOP-DNAVDAU.home>
2016-12-02 16:56:31 +0100

    added Comments

--- src/eu/thecell/flyby/DrawPanel.java
+++ src/eu/thecell/flyby/DrawPanel.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.thecell.flyby;
 
-import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 /**
- *
+ * Extends JPanel, adds the GameObjects to the Class for drawing.
  * @author simon
  */
 public class DrawPanel extends JPanel
 {
     private boolean inDebug = false;
-    private int leftTest = 0;
-    private int rightTest = 5;
     private GameObjects allGameObjects;
     
     public DrawPanel()
@@ -27,21 +19,33 @@ public class DrawPanel extends JPanel
     
     public DrawPanel(boolean debug)
     {
-//	this.setPreferredSize(new Dimension(1024, 768));
 	this(debug, new GameObjects());
     }
     
+    /**
+     * Add GameObjects at inizialisation, it is possible to change gameobjects later on.
+     * @param debug true = debugmode;
+     * @param gameObjs 
+     */
     public DrawPanel(boolean debug, GameObjects gameObjs)
     {
 	this.inDebug = debug;
 	this.allGameObjects = gameObjs;
     }
     
+    /**
+     * Replace list of GameObjects with a new List.
+     * @param gameObjs 
+     */
     public void setGameObjects(GameObjects gameObjs)
     {
 	this.allGameObjects = gameObjs;
     }
-
+    
+    /**
+     * Draw and then delegate the Gameobjects to draw them aswell.
+     * @param g 
+     */
     @Override
     protected void paintComponent(Graphics g)
     {
