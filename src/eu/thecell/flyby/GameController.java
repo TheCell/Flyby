321bd2b71c13072489e357b8d8dfc91f4586233a
simon <simon@DESKTOP-DNAVDAU.home>
2016-12-02 16:47:55 +0100

    removed default Comment

--- src/eu/thecell/flyby/GameController.java
+++ src/eu/thecell/flyby/GameController.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package eu.thecell.flyby;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
-
 
 /**
  * Delegates the draw, updates Physics, starts the PlayerActionsHandler,
