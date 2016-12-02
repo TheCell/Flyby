108089b2f48b8a8cc014137db1578063053a493b
simon <simon@DESKTOP-DNAVDAU.home>
2016-12-02 17:49:22 +0100

    added Comments

--- src/eu/thecell/flyby/Healthpoints.java
+++ src/eu/thecell/flyby/Healthpoints.java
@@ -2,14 +2,8 @@ package eu.thecell.flyby;
 
 import java.util.Objects;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
+ * Keeps track of the Healthpoints, default Value is 100.
  * @author simon
  */
 public class Healthpoints
@@ -21,26 +15,46 @@ public class Healthpoints
 	this( 100 );
     }
     
+    /**
+     * Set the Healthpoints.
+     * @param health 
+     */
     public Healthpoints(int health)
     {
 	this.healthpoints = health;
     }
-
+    
+    /**
+     * Override the Healthpoints with new Value.
+     * @param health 
+     */
     public void setHealthpoints(int health)
     {
 	this.healthpoints = health;
     }
     
-    public void damageHealth(int health)
+    /**
+     * Reduce the Health by the given amount of Health.
+     * @param damage 
+     */
+    public void damageHealth(int damage)
     {
-	this.healthpoints -= health;
+	this.healthpoints -= damage;
     }
     
+    /**
+     * The currently saved Healthpoints
+     * @return int actual Healthpoints
+     */
     public int getHealth()
     {
 	return this.healthpoints;
     }
     
+    /**
+     * Checks if Health is still above 0.
+     * @return boolean true if still alive
+     */
     public boolean isAlive()
     {
 	if(this.healthpoints > 0)
@@ -53,6 +67,11 @@ public class Healthpoints
 	}
     }
     
+    /**
+     * Checks is Health is the same as the given Object.
+     * @param o
+     * @return 
+     */
     @Override
     public final boolean equals(Object o)
     {
@@ -70,6 +89,10 @@ public class Healthpoints
 	return this.healthpoints == tempHp.healthpoints;
     }
     
+    /**
+     * returns hashCode
+     * @return 
+     */
     @Override
     public final int hashCode()
     {
